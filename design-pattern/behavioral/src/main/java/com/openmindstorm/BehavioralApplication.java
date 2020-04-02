fcb3a5b8af414f2e64be5ffaa588e9527a4e8156
liuyiwei <liuyiwei.dlbc@gmail.com>
2020-04-02 09:54:05 +0800

    refactor

--- design-pattern/behavioral/src/main/java/com/openmindstorm/BehavioralApplication.java
+++ design-pattern/behavioral/src/main/java/com/openmindstorm/BehavioralApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesignPatternApplication {
+public class BehavioralApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DesignPatternApplication.class, args);
+        SpringApplication.run(BehavioralApplication.class, args);
     }
 
 }
---
+++ design-pattern/structural/src/main/java/com/openmindstorm/StructuralApplication.java
@@ -0,0 +1,13 @@
+package com.openmindstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructuralApplication.class, args);
+    }
+
+}
